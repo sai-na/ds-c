9a304a551a7bf39d960c9f931737cf567f2daeae
sainath378 <92171156+sai-na@users.noreply.github.com>
2022-10-14 05:16:04 +0530

    code formatted

--- queue.c
+++ queue.c
@@ -1,119 +1,96 @@
-#include<stdio.h>
+#include <stdio.h>
 #define MaxSize 5
-int queue [5];
+int queue[5];
 int rear = -1;
-int front = 0 ;
+int front = 0;
 
+void display()
+{
 
-
-
-
-
-
-void display(){
-
-int i;
-printf("The queue is :");
-for(i=front; i<=rear ; i++)
-printf("%d \n" , queue[i]);
-
+	int i;
+	printf("The queue is :");
+	for (i = front; i <= rear; i++)
+		printf("%d \n", queue[i]);
 }
 
+int isFull()
+{
 
-
-
-
-
-
-int isFull(){
-
-if(rear >= MaxSize -1) // array index
-	return 1 ;
-else return 0 ;
-
+	if (rear >= MaxSize - 1) // array index
+		return 1;
+	else
+		return 0;
 }
 
+int isEmpty()
+{
 
-
-
-int isEmpty(){
-
-if(front > rear)
-	return 1;
-else return 0;
-
+	if (front > rear)
+		return 1;
+	else
+		return 0;
 }
 
+void enqueue(int a)
+{
 
+	if (isFull())
+		printf("Queue Overfolw");
 
-void enqueue(int a ){
-
-if( isFull())
-  printf("Queue Overfolw");
-
-else{
-rear++;
-queue [rear] = a ;
-display();
-
+	else
+	{
+		rear++;
+		queue[rear] = a;
+		display();
+	}
 }
 
+void dequeue()
+{
 
-
+	if (isEmpty())
+		printf("Queue underflow");
+	else
+	{
+		printf(" The popped element element is %d", queue[front]);
+		front++;
+		display();
+	}
 }
 
-void dequeue() {
+void main()
+{
 
-if(isEmpty())
-	printf("Queue underflow");
-else {
-printf(" The popped element element is %d" , queue[front]);
-	front++;
- 	display();
+	int choice, a;
+
+	do
+	{
+
+		printf("Enter your choice 1. Enqueue 2. Dequeue 3. Display 4 .Exit");
+
+		scanf("%d", &choice);
+
+		switch (choice)
+		{
+
+		case 1:
+			printf("Enter element to be added to the queue ");
+			scanf("%d", &a);
+			enqueue(a);
+			break;
+
+		case 2:
+			dequeue();
+			break;
+
+		case 3:
+			display();
+			break;
+
+		case 4:
+			break;
+		}
+	}
+
+	while (choice != 4);
 }
-
-
-}
-
-
-void main(){
-
-int choice , a ;
-
-do{
-
-printf("Enter your choice 1. Enqueue 2. Dequeue 3. Display 4 .Exit");
-
-scanf("%d" , &choice);
-
-switch(choice){
-
-case 1 : printf("Enter element to be added to the queue ");
- 	 scanf("%d" , &a);
- 	 enqueue(a);
- 	 break;
-
-case 2 :  dequeue(); 
-	break;
-
-case 3: display(); 
-	 break;
-
-case 4 : break;
-
-
-
-
-}
-}
-
-
-while(choice!= 4);
-
-}
-
-
-
-
-
-
