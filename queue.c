b942daf045f2d5dfe9e5132ec49fb31d32687dd5
sainath378 <92171156+sai-na@users.noreply.github.com>
2022-12-18 00:21:24 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,98 +1,97 @@
-#include<stdio.h>
+#include <stdio.h>
 #define MaxSize 5
-int queue[MaxSize],front=-1,rear=-1;
+int queue[MaxSize], front = -1, rear = -1;
 
+// display
 
-
-
-//display
-
-void display (){
-	if(isEmpty()) 	printf("\n Queue underflow error : the queue is empty \n");
-	else{
+void display()
+{
+	if (isEmpty())
+		printf("\n Queue underflow error : the queue is empty \n");
+	else
+	{
 		printf("\n The queue is :");
-		for(int i = front ; i<=rear ; i++) printf("%d ", queue[i] );
+		for (int i = front; i <= rear; i++)
+			printf("%d ", queue[i]);
 		printf("\n");
 	}
 }
 
-
-int isEmpty(){
-	if(  (front>rear) )
+int isEmpty()
+{
+	if ((front > rear))
 		return 1;
 	else
 		return 0;
 }
 
-int isFull(){
-	if(rear >=(MaxSize-1))
+int isFull()
+{
+	if (rear >= (MaxSize - 1))
 		return 1;
 	else
-	 	return 0;
-	 	
-	 	}
+		return 0;
+}
 
-
-
-
-
-
-
-
-	
-	
-	
 // enqueue
-void enqueue(int data ){
-	
-	if(isFull()) printf("Queue is full");
-	else{
-		if(front == -1) front = 0;				
+void enqueue(int data)
+{
+
+	if (isFull())
+		printf("Queue is full");
+	else
+	{
+		if (front == -1)
+			front = 0;
 		rear++;
 		queue[rear] = data;
 		display();
-	
 	}
 }
 
-//dequeue
+// dequeue
 
-void dequeue()	{
+void dequeue()
+{
 
-	if(isEmpty()) 	printf("\n Queue underflow error : the queue is empty \n");
-	else {
-		printf("\n The removed element is %d \n " , queue[front]);
-		front ++ ;
+	if (isEmpty())
+		printf("\n Queue underflow error : the queue is empty \n");
+	else
+	{
+		printf("\n The removed element is %d \n ", queue[front]);
+		front++;
 		display();
+	}
 }
-}
 
-
-
-void main(){
+void main()
+{
 	printf("Enter your choice:");
 
-	int flag,data;
-	do{
+	int flag, data;
+	do
+	{
 		printf("1.Enqueu\n2.Dequeue\n3.Display\n4.Exit\n");
 		printf("enter your choice: ");
-		scanf("%d",&flag);
-		switch(flag){
-			case 1:
-				printf("enter data to be added: ");
-				scanf("%d",&data);
-				enqueue(data);
-			       break;
-			case 2:dequeue();
-			       break;
-			case 3:display();
-				break;
-			case 4:
-				break;;
-			default:printf("invalid choice\n");
+		scanf("%d", &flag);
+		switch (flag)
+		{
+		case 1:
+			printf("enter data to be added: ");
+			scanf("%d", &data);
+			enqueue(data);
+			break;
+		case 2:
+			dequeue();
+			break;
+		case 3:
+			display();
+			break;
+		case 4:
+			break;
+			;
+		default:
+			printf("invalid choice\n");
 		}
-		}
-		while(flag!=4);
-			       	       	
-	}
-	
+	} while (flag != 4);
+}
