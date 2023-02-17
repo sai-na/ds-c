e607a34a36aa5f34570d59eb297b1f719bb1dcb0
sainath378 <92171156+sai-na@users.noreply.github.com>
2023-02-17 20:18:40 +0530

    Update dqueue.c

--- exam/dqueue.c
+++ exam/dqueue.c
@@ -1,126 +1,200 @@
 #include <stdio.h>
-#include <conio.h>
-#include <stdlib.h>
-#define size 5
+
+#define MAX_SIZE 5
+
+int arr[MAX_SIZE]; // Static array to store elements
+int front = -1;    // Index of the front of the deque
+int rear = -1;     // Index of the rear of the deque
+int size = 0;      // Current size of the deque
+
+// Function to check if the deque is empty
+int isEmpty()
+{
+    return (size == 0);
+}
+
+// Function to check if the deque is full
+int isFull()
+{
+    return (size == MAX_SIZE);
+}
+
+// Function to add an element to the front of the deque
+void addFront(int value)
+{
+    if (isFull())
+    {
+        printf("Deque is full. Cannot add element.\n");
+        return;
+    }
+
+    if (front == -1)
+    {
+        front = 0;
+        rear = 0;
+    }
+    else if (front == 0)
+    {
+        front = MAX_SIZE - 1;
+    }
+    else
+    {
+        front--;
+    }
+    arr[front] = value;
+    size++;
+
+    printf("added %d at front ", value);
+}
+
+// Function to add an element to the rear of the deque
+void addRear(int value)
+{
+    if (isFull())
+    {
+        printf("Deque is full. Cannot add element.\n");
+        return;
+    }
+    if (rear == -1)
+    {
+        front = 0;
+        rear = 0;
+    }
+    else if (rear == MAX_SIZE - 1)
+    {
+        rear = 0;
+    }
+    else
+    {
+        rear++;
+    }
+    arr[rear] = value;
+    size++;
+    printf("added %d at rear ", value);
+}
+
+// Function to remove an element from the front of the deque
+void removeFront()
+{
+
+    int value = arr[front];
+    if (isEmpty())
+    {
+        printf("Deque is empty. Cannot remove element.\n");
+        return;
+    }
+    if (front == rear)
+    {
+        front = -1;
+        rear = -1;
+    }
+    else if (front == MAX_SIZE - 1)
+    {
+        front = 0;
+    }
+    else
+    {
+        front++;
+    }
+    size--;
+
+    printf("removed %d from front ", value);
+}
+
+// Function to remove an element from the rear of the deque
+void removeRear()
+{
+
+    int value = arr[rear];
+    if (isEmpty())
+    {
+        printf("Deque is empty. Cannot remove element.\n");
+        return;
+    }
+    if (front == rear)
+    {
+        front = -1;
+        rear = -1;
+    }
+    else if (rear == 0)
+    {
+        rear = MAX_SIZE - 1;
+    }
+    else
+    {
+        rear--;
+    }
+    size--;
+
+    printf("removed %d from rear ", value);
+}
+
+// Function to get the element at the front of the deque
+int getFront()
+{
+    if (isEmpty())
+    {
+        printf("Deque is empty. Cannot get element.\n");
+        return -1;
+    }
+    return arr[front];
+}
+
+// Function to get the element at the rear of the deque
+int getRear()
+{
+    if (isEmpty())
+    {
+        printf("Deque is empty. Cannot get element.\n");
+        return -1;
+    }
+    return arr[rear];
+}
+
+// Function to display the deque
+void display()
+{
+    if (isEmpty())
+    {
+        printf("Deque is empty.\n");
+        return;
+    }
+    int i;
+    printf("Deque: ");
+    for (i = front; i != rear; i = (i + 1) % MAX_SIZE)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("%d\n", arr[i]);
+}
 
 int main()
 {
-    int arr[size], R = -1, F = 0, te = 0, ch, n, i, x;
+    addFront(10);
+    display();
+    addRear(20);
+    display();
+    addFront(30);
+    display();
+    addRear(40);
+    display();
+    addFront(50);
+    display();
+    addRear(60);
+    display();
 
-    for (;;) // An infinite loop
-    {
-        system("cls"); // for clearing the screen
-        printf("F=%d  R=%d\n\n", F, R);
-        printf("1. Add Rear\n");
-        printf("2. Delete Rear\n");
-        printf("3. Add Front\n");
-        printf("4. Delete Front\n");
-        printf("5. Display\n");
-        printf("6. Exit\n");
-        printf("Enter Choice: ");
-        scanf("%d", &ch);
+    removeFront();
+    display();
+    removeRear();
+    display();
 
-        switch (ch)
-        {
-        case 1:
-            if (te == size)
-            {
-                printf("Queue is full");
-                getch(); // pause the loop to see the message
-            }
-            else
-            {
-                printf("Enter a number ");
-                scanf("%d", &n);
-                R = (R + 1) % size;
-                arr[R] = n;
-                te = te + 1;
-            }
-            break;
+    removeFront();
+    display();
+    removeRear();
+    display();
 
-        case 2:
-            if (te == 0)
-            {
-                printf("Queue is empty");
-                getch(); // pause the loop to see the message
-            }
-            else
-            {
-                if (R == -1)
-                {
-                    R = size - 1;
-                }
-                printf("Number Deleted From Rear End = %d", arr[R]);
-                R = R - 1;
-                te = te - 1;
-                getch(); // pause the loop to see the number
-            }
-            break;
-
-        case 3:
-            if (te == size)
-            {
-                printf("Queue is full");
-                getch(); // pause the loop to see the message
-            }
-            else
-            {
-                printf("Enter a number ");
-                scanf("%d", &n);
-                if (F == 0)
-                {
-                    F = size - 1;
-                }
-                else
-                {
-                    F = F - 1;
-                }
-                arr[F] = n;
-                te = te + 1;
-            }
-            break;
-
-        case 4:
-            if (te == 0)
-            {
-                printf("Queue is empty");
-                getch(); // pause the loop to see the message
-            }
-            else
-            {
-                printf("Number Deleted From Front End = %d", arr[F]);
-                F = (F + 1) % size;
-                te = te - 1;
-                getch(); // pause the loop to see the number
-            }
-            break;
-
-        case 5:
-            if (te == 0)
-            {
-                printf("Queue is empty");
-                getch(); // pause the loop to see the message
-            }
-            else
-            {
-                x = F;
-                for (i = 1; i <= te; i++)
-                {
-                    printf("%d ", arr[x]);
-                    x = (x + 1) % size;
-                }
-                getch(); // pause the loop to see the numbers
-            }
-            break;
-
-        case 6:
-            exit(0);
-            break;
-
-        default:
-            printf("Wrong Choice");
-            getch(); // pause the loop to see the message
-        }
-    }
-    return 0;
+    removeFront();
+    display();
+    removeRear();
+    display();
 }
