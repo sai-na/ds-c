caf029110dbee12670973e8627769c989c890746
sainath378 <92171156+sai-na@users.noreply.github.com>
2023-02-18 12:49:23 +0530

    Update polynomial.c

--- exam-g/polynomial.c
+++ exam-g/polynomial.c
@@ -2,7 +2,7 @@
 
 void add_polynomials(int poly1[], int poly2[], int result[], int size)
 {
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i <= size; i++)
     {
         result[i] = poly1[i] + poly2[i];
     }
@@ -19,21 +19,33 @@ int main()
     printf("Enter the coefficients of the first polynomial: ");
     for (int i = 0; i <= size; i++)
     {
+        printf("coefficient of x^%d =", i);
         scanf("%d", &poly1[i]);
     }
 
     printf("Enter the coefficients of the second polynomial: ");
     for (int i = 0; i <= size; i++)
     {
+        printf("coefficient of x^%d =", i);
+
         scanf("%d", &poly2[i]);
     }
 
-    add_polynomials(poly1, poly2, result, size + 1);
+    add_polynomials(poly1, poly2, result, size);
 
     printf("Result: ");
-    for (int i = 0; i <= size; i++)
+
+        for (int i = 0; i <= size; i++)
     {
-        printf("%d ", result[i]);
+        // printf("%d ", result[i]);
+        printf(" %d(x^%d) ", result[i], i);
+
+        if (i == size)
+        {
+            printf("\n");
+            break;
+        }
+        printf("+");
     }
 
     return 0;
